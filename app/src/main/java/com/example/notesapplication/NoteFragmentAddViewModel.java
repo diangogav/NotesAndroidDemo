9a0c4bb1ec5f664cada08264826d264e51007421
DiangoGav <diango.gavidia@sitiouno.com>
2020-03-15 18:19:42 -0400

    Se implementa ViewModel para Note

--- app/src/main/java/com/example/notesapplication/NoteFragmentAddViewModel.java
+++ app/src/main/java/com/example/notesapplication/NoteFragmentAddViewModel.java
@@ -1,7 +1,27 @@
 package com.example.notesapplication;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class NoteFragmentAddViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteFragmentAddViewModel extends AndroidViewModel {
+
+    private NoteRepository noteRepository;
+    private LiveData<List<NoteEntity>> allNotes;
+
+    public NoteFragmentAddViewModel(@NonNull Application application) {
+        super(application);
+        noteRepository = new NoteRepository(application);
+        allNotes = noteRepository.getAllNotes();
+    }
+
+    LiveData<List<NoteEntity>> getAllNotes() { return allNotes; }
+
+    public void addNote(NoteEntity note) {
+        noteRepository.insertNote(note);
+    }
 }
