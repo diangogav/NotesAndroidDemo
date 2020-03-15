5eb137e10a44949ba7acb8651908a873940d6e33
DiangoGav <diango.gavidia@sitiouno.com>
2020-03-15 11:48:07 -0400

    Se agrega un activity de tipo bottom navigation y se comunica con el MainActivity a través de un Intent

--- app/src/main/java/com/example/notesapplication/MainActivity.java
+++ app/src/main/java/com/example/notesapplication/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.notesapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(MainActivity.this, DashboardActivity.class);
+        startActivity(i);
     }
 }
