1075b8275e597e07d44df1c13f591c30279921ee
DiangoGav <diango.gavidia@sitiouno.com>
2020-03-15 15:17:18 -0400

    Se agrega método para agregar una nota de forma asíncrona

--- app/src/main/java/com/example/notesapplication/NoteRepository.java
+++ app/src/main/java/com/example/notesapplication/NoteRepository.java
@@ -1,6 +1,7 @@
 package com.example.notesapplication;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 
@@ -18,5 +19,23 @@ public class NoteRepository {
 
     public LiveData<List<NoteEntity>> getAllNotes() {
         return this.allNotes;
+    }
+
+    public void insertNote(NoteEntity note) {
+        new InsertAsyncTask(noteDao).execute(note);
+    }
+
+    private static class InsertAsyncTask extends AsyncTask<NoteEntity, Void, Void> {
+        private NoteDao noteDaoAsyncTask;
+
+        InsertAsyncTask(NoteDao dao) {
+            noteDaoAsyncTask = dao;
+        }
+
+        @Override
+        protected Void doInBackground(NoteEntity... noteEntities) {
+            noteDaoAsyncTask.insert(noteEntities[0]);
+            return null;
+        }
     }
 }
